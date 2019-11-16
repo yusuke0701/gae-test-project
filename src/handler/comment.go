7ae89c2d0c7b43f7fa1ff2951e1a2f2c09d28529
YusukeNakashima <yuusukerego@yahoo.co.jp>
2019-11-16 09:30:20 +0000

    move to go1.12

---
+++ src/handler/comment.go
@@ -0,0 +1,41 @@
+package handler
+
+import (
+	"gae-test-project/model"
+	"gae-test-project/store"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+// Comments is handler bundle
+func Comments(g *gin.RouterGroup) {
+	g.PUT("", insertComment)
+	g.GET("/:id", getComment)
+}
+
+func insertComment(ctx *gin.Context) {
+	body, ok := ctx.GetQuery("body")
+	if !ok {
+		ctx.String(http.StatusBadRequest, "body is required")
+		return
+	}
+	commnet := &model.Comment{ID: "10", Body: body}
+
+	if err := store.Comment.Insert(ctx.Request.Context(), commnet); err != nil {
+		ctx.String(http.StatusInternalServerError, err.Error())
+		return
+	}
+	ctx.JSON(http.StatusOK, commnet)
+}
+
+func getComment(ctx *gin.Context) {
+	commentID := ctx.Param("id")
+
+	commnet, err := store.Comment.Get(ctx.Request.Context(), commentID)
+	if err != nil {
+		ctx.String(http.StatusInternalServerError, err.Error())
+		return
+	}
+	ctx.JSON(http.StatusOK, commnet)
+}
--- src/store/comment.go
+++ src/store/comment.go
@@ -2,88 +2,39 @@ package store
 
 import (
 	"context"
-	"gae-test-project/src/model"
+	"gae-test-project/model"
+	"gae-test-project/util"
 
-	"go.mercari.io/datastore/aedatastore"
-	"go.mercari.io/datastore/boom"
+	"cloud.google.com/go/datastore"
 )
 
-// CommentStore は、CommentのCRUDを担保する
-type CommentStore struct {
-	kindName string
-	b        *boom.Boom
+// Comment は、コメントのDB操作を担保する
+var Comment *comment
+
+func init() {
+	Comment = &comment{
+		kind: "comment",
+	}
 }
 
-// NewCommentStore は、CommentStoreを用意する
-func NewCommentStore(c context.Context) (*CommentStore, error) {
-	cli, err := aedatastore.FromContext(c) // Memo: 2018/12/29 aedatastore であれば cli.Close() を呼び出す必要はない
-	if err != nil {
-		return nil, err
-	}
-	b := boom.FromClient(c, cli)
-	return &CommentStore{
-		kindName: b.Kind(model.Comment{}),
-		b:        b,
-	}, nil
+type comment struct {
+	kind string
 }
 
-// Get は、Commentを一件取得する
-func (store *CommentStore) Get(email string, id int64) (*model.Comment, error) {
-	comment, err := model.NewComment(store.b, email, id, "", "")
-	if err != nil {
-		return nil, err
-	}
-	if err := store.b.Get(comment); err != nil {
-		return nil, err
-	}
-	return comment, nil
+func (cStore *comment) newKey(id string) *datastore.Key {
+	return datastore.NameKey(cStore.kind, id, nil)
 }
 
-// ListByEmail は、emailを元にCommentを複数件取得する
-func (store *CommentStore) ListByEmail(email string) ([]*model.Comment, error) {
-	user := model.NewUser(email, "", "", "")
-	key, err := store.b.KeyError(user)
-	if err != nil {
-		return nil, err
-	}
-
-	q := store.b.NewQuery(store.kindName).Ancestor(key)
-	comments := make([]*model.Comment, 0)
-	if _, err := store.b.GetAll(q, &comments); err != nil {
-		return nil, err
-	}
-	return comments, nil
-}
-
-// Insert は、Commentを新規作成する
-func (store *CommentStore) Insert(email, title, body string) (*model.Comment, error) {
-	comment, err := model.NewComment(store.b, email, 0, title, body) // idに0を入れることで、Datastore側でよしなにやってくれる
-	if err != nil {
-		return nil, err
-	}
-	if _, err := store.b.Put(comment); err != nil {
-		return nil, err
-	}
-	return comment, nil
-}
-
-// Update は、Commentを更新する
-func (store *CommentStore) Update(id int64, email, title, body string) (*model.Comment, error) {
-	comment, err := model.NewComment(store.b, email, id, title, body)
-	if err != nil {
-		return nil, err
-	}
-	if _, err := store.b.Put(comment); err != nil {
-		return nil, err
-	}
-	return comment, nil
-}
-
-// Delete は、Commentを削除する
-func (store *CommentStore) Delete(id int64) error {
-	comment, err := model.NewComment(store.b, "", id, "", "")
-	if err != nil {
+// Insert は、コメントを一件保存する
+func (cStore *comment) Insert(ctx context.Context, c *model.Comment) error {
+	if _, err := util.DatastoreClient.Put(ctx, cStore.newKey(c.ID), c); err != nil {
 		return err
 	}
-	return store.b.Delete(comment)
+	return nil
+}
+
+// Get は、コメントを一件取得する
+func (cStore *comment) Get(ctx context.Context, id string) (c *model.Comment, err error) {
+	err = util.DatastoreClient.Get(ctx, cStore.newKey(id), c)
+	return
 }
