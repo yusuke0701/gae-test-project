1c665bf3ed5b6915940b942f140134fd9feaa9c9
YusukeNakashima <yuusukerego@yahoo.co.jp>
2019-12-09 14:01:40 +0000

    signedURLの画面を用意

---
+++ client/src/components/SignedURL.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <input v-model="csvFileName" size="20" />
+    <button v-on:click="downlaodCSV">CSVダウンロード</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "SignedURL",
+  data: function() {
+    return {
+      csvFileName: ""
+    };
+  },
+  methods: {
+    downlaodCSV: function() {
+      const apiURL = "/api/url/csv-download";
+      axios
+        .get(apiURL + "/" + this.csvFileName)
+        .then(response => response.text())
+        .then(textData => window.open(textData))
+        .catch(error => console.log(error));
+    }
+  }
+};
+</script>
