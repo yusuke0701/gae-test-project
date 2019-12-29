b65cf008e7f79bb5e2ef9407a1d959ae6a579ad2
yusuke-home <yuusukerego@yahoo.co.jp>
2019-12-29 17:45:36 +0900

    setup.shの作成

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -3,7 +3,13 @@
 cd `dirname $0`
 
 export GO111MODULE=off
-
 go get -u github.com/codegangsta/gin
 go get -u golang.org/x/tools/cmd/goimports
 go get -u golang.org/x/lint/golint
+
+cd ../client
+npm install
+
+cd ../server
+export GO111MODULE=on
+go mod download
