535a253361b53a03198cc523c8eb5ee65986d0b0
yusuke-home <yuusukerego@yahoo.co.jp>
2019-12-30 23:44:47 +0900

    [chore]deploy.shを使いやすくした

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,11 +1,24 @@
 #! /bin/sh -eux
 
+# GAEへデプロイするためのシェルスクリプト
+# (例) ./scripts/deploy.sh -a hoge-hoge-123456789 -v from-pc
+
 cd `dirname $0`
 
 sh ./prepush.sh
+
+while getopts a:v: OPT
+do
+	case $OPT in
+		"a" ) PROJECT_ID="$OPTARG";;
+		"v" ) VERSION="$OPTARG";;
+		* ) echo "Usage: $CMDNAME -a=<project_id> -v=<version>"
+			exit 1 ;;
+	esac
+done
 
 cd ../client
 npm run build
 
 cd ../server
-gcloud app deploy --quiet app.yaml backend.yaml modules/dispatch.yaml --version=from-pc
+gcloud app deploy app.yaml backend.yaml modules/dispatch.yaml --project=$PROJECT_ID --version=$VERSION
