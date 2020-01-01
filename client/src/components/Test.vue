9aef4c49fcb4d9eaf5b6294841865eb0879b2655
yusuke-home <yuusukerego@yahoo.co.jp>
2020-01-02 05:12:56 +0900

    [client]親子での値渡し動作確認

--- client/src/components/Test.vue
+++ client/src/components/Test.vue
@@ -1,31 +1,42 @@
 <template>
   <div class="test">
-    <h1>{{ msg }}</h1>
-    <span v-if="seen">Now you see me</span>
-    <ol>
-      <li v-for="todo in todos" v-bind:key="todo.id">{{todo.text}}</li>
-    </ol>
-    <button @click="reverseMessage">Reverse Message</button>
     <div>
-      <button @click="onClick('ネイティブDOMイベントもとれる',$event)">click</button>
-      {{clickResult}}
+      <h2>Vueの基本</h2>
+      <p>{{ msg }}</p>
+      <span v-if="seen">Now you see me</span>
+      <ol>
+        <li v-for="todo in todos" v-bind:key="todo.id">{{todo.text}}</li>
+      </ol>
     </div>
     <div>
-      <input @change="onChange" placeholder="change" />
-      {{changeResult}}
+      <h2>ボタンイベント</h2>
+      <button @click="reverseMessage">Reverse Message</button>
+      <div>
+        <button @click="onClick('ネイティブDOMイベントもとれる',$event)">click</button>
+        {{clickResult}}
+      </div>
+      <div>
+        <input @change="onChange" placeholder="change" />
+        {{changeResult}}
+      </div>
+      <div>
+        <input @input="onInput" placeholder="input" />
+        {{inputResult}}
+      </div>
+      <div>
+        <input @keyup="onKeyup" placeholder="keyup" />
+        {{keyupResult}}
+      </div>
     </div>
     <div>
-      <input @input="onInput" placeholder="input" />
-      {{inputResult}}
-    </div>
-    <div>
-      <input @keyup="onKeyup" placeholder="keyup" />
-      {{keyupResult}}
+      <h2>値の受け渡し</h2>
+      <child :pval="parentValue" @add="addValueParent"></child>
     </div>
   </div>
 </template>
 
 <script>
+import Child from "./TestChild";
 export default {
   name: "Test",
   data: function() {
@@ -40,8 +51,12 @@ export default {
       clickResult: "",
       changeResult: "",
       inputResult: "",
-      keyupResult: ""
+      keyupResult: "",
+      parentValue: 100
     };
+  },
+  components: {
+    child: Child
   },
   methods: {
     reverseMessage() {
@@ -63,6 +78,9 @@ export default {
     },
     onKeyup(e) {
       this.keyupResult = e.target.value;
+    },
+    addValueParent(value) {
+      this.parentValue += value;
     }
   }
 };
---
+++ client/src/components/TestChild.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    {{pval}}
+    <button @click="addValueChild()">ADD</button>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["pval"],
+  methods: {
+    addValueChild() {
+      this.$emit("add", 10);
+    }
+  }
+};
+</script>
